ce79417052a0fd5301b87fc77aaf27f681eb95fa
CarlosEduardo7700 <rm552164@fiap.com.br>
2023-10-30 17:20:54 -0300

    save

--- scripts/script_consulta_forma_pagto.py
+++ scripts/script_consulta_forma_pagto.py
@@ -8,45 +8,26 @@ fake = Faker('pt_BR')
 conn = oracledb.connect(user="rm552164", password="fiap23", dsn="oracle.fiap.com.br/orcl")
 cur = conn.cursor()
 
-# Data mínima e máxima para as datas de início
 data_min = datetime(2010, 1, 1)
 data_max = datetime(2022, 12, 31)
-
-# Contador para ID do paciente
 id_counter = 1
 
-def gerar_letra_aleatoria():
-    letras_possiveis = ['A', 'C', 'P']
-    letra_aleatoria = random.choice(letras_possiveis)
-    return letra_aleatoria
-
-# Quantidade de registros a serem criados
 for _ in range(500000):
     id_consulta_pagto = id_counter 
     id_consulta = id_counter 
     id_paciente = id_counter 
    
-
     cur.execute("SELECT ID_UNID_HOSPITAL FROM T_RHSTU_CONSULTA WHERE ID_CONSULTA = :ID_CONSULTA", ID_CONSULTA = id_consulta)
-    # Recupere o dado do SELECT
     dado_selecionado = cur.fetchone()
-
     id_unid_hospital = dado_selecionado[0]
-    print(dado_selecionado[0])
 
     cur.execute("SELECT ID_PACIENTE_PS FROM T_RHSTU_PACIENTE_PLANO_SAUDE WHERE ID_PACIENTE = :ID_PACIENTE", ID_PACIENTE = id_paciente)
-    # Recupere o dado do SELECT
     dado_selecionado = cur.fetchone()
-    print(dado_selecionado[0])
-    
     id_paciente_ps = dado_selecionado[0]
+    
     id_forma_pagto = random.randint(1, 6)
     dt_pagto_consulta = fake.date_between_dates(data_min, data_max).strftime('%d-%b-%Y').upper()
-    st_pagto_consulta = gerar_letra_aleatoria()
-
-
-
-
+    st_pagto_consulta = random.choice(['A', 'C', 'P'])
     dt_fim = None
     dt_cadastro = "SYSDATE"
     nm_usuario = "USER"
