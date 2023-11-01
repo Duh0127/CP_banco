8fb5c6c75c9498c8a701172e97cc8fd42e368018
Kauezin05 <kauezinho_naruto@hotmail.com>
2023-10-31 22:31:58 -0300

    CSV PRECRICAO2

--- scripts/script_prescricao_medica_PROBLEMA.py
+++ scripts/script_prescricao_medica_PROBLEMA.py
@@ -1,41 +1,48 @@
 import random
 import csv
 from faker import Faker
-import datetime
+from datetime import datetime
+from pegar_unid_consulta import pegar_ids
  
-fake = Faker('pt_BR')
+fake = Faker("pt_BR")
  
-data_min = datetime.datetime(2022, 1, 1, 0, 0, 0)
-data_max = datetime.datetime(2023, 12, 31, 23, 59, 59)
+data_min = datetime(2022, 1, 1, 0, 0, 0)
+data_max = datetime(2023, 12, 31, 23, 59, 59)
 id_counter = 1
  
 # Nome do arquivo CSV de saída
-output_file = 'D:/dados_prescricao.csv'
+output_file = 'dados_prescricao_medica.csv'
  
 with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
-    fieldnames = ['ID_UNID_HOSPITAL', 'ID_CONSULTA', 'ID_PACIENTE', 'ID_FUNC', 'DT_HR_CONSULTA', 'NR_CONSULTORIO']
+    fieldnames = ['ID_PRESCRICAO_MEDICA', 'ID_MEDICAMENTO', 'DS_POSOLOGIA', 'DS_VIA', 'DS_OBSERVACAO_USO', 'QT_MEDICAMENTO', 'ID_UNID_HOSPITAL', 'ID_CONSULTA']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
  
     writer.writeheader()
  
-    for _ in range(1, 10000000):
-        id_unid_hospital = random.randint(1, 20000)
-        id_consulta = id_counter
-        id_paciente = id_counter
-        id_funcionario = random.randint(1, 61000)
-        dt_hr_consulta = fake.date_time_between_dates(data_min, data_max).strftime('%d-%b-%Y %H:%M:%S').upper()
-        nr_consultorio = random.randint(1, 99999)
+    for _ in range(499999):
+        id_prescricao_medica = id_counter
+        id_medicamento = random.randint(1, 150000)
+        ds_posologia = fake.sentence(nb_words=4)
+        ds_via = random.choice(["ORAL","PARENTAL","SUBCUTÂNEA","NASAL","RETAL","INTRA-VESICAL","NEBULIZAÇÃO","OCULAR"])
+        observacao_uso = fake.sentence(nb_words=6)
+        qt_medicamento = random.randint(1, 60)
+ 
+        # Suponho que você já tenha uma função pegar_ids definida em 'pegar_unid_consulta.py'
+        id_unid_hospital, id_consulta = pegar_ids(id_counter)
  
         writer.writerow({
+            'ID_PRESCRICAO_MEDICA': id_prescricao_medica,
+            'ID_MEDICAMENTO': id_medicamento,
+            'DS_POSOLOGIA': ds_posologia,
+            'DS_VIA': ds_via,
+            'DS_OBSERVACAO_USO': observacao_uso,
+            'QT_MEDICAMENTO': qt_medicamento,
             'ID_UNID_HOSPITAL': id_unid_hospital,
-            'ID_CONSULTA': id_consulta,
-            'ID_PACIENTE': id_paciente,
-            'ID_FUNC': id_funcionario,
-            'DT_HR_CONSULTA': dt_hr_consulta,
-            'NR_CONSULTORIO': nr_consultorio
+            'ID_CONSULTA': id_consulta
         })
  
-        print(id_counter)
+        print(id_prescricao_medica, id_medicamento, ds_posologia, ds_via, observacao_uso, qt_medicamento, id_unid_hospital, id_consulta)
+ 
         id_counter += 1
  
 print(f'Dados gravados em {output_file}')
