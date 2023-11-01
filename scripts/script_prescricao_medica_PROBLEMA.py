62bf0e138069b3057ef97edb6b089ef2b2d0aa53
Kauezin05 <kauezinho_naruto@hotmail.com>
2023-10-31 22:22:58 -0300

    CSV prescricao

--- scripts/script_prescricao_medica_PROBLEMA.py
+++ scripts/script_prescricao_medica_PROBLEMA.py
@@ -1,48 +1,41 @@
-from datetime import datetime
 import random
-import oracledb
+import csv
 from faker import Faker
-from pegar_unid_consulta import pegar_ids
-
-fake = Faker("pt_BR")
-conn = oracledb.connect(user="rm552164", password="fiap23", dsn="oracle.fiap.com.br/orcl")
-cur = conn.cursor()
-
-data_min = datetime(2022, 1, 1, 0, 0, 0)
-data_max = datetime(2023, 12, 31, 23, 59, 59)
+import datetime
+ 
+fake = Faker('pt_BR')
+ 
+data_min = datetime.datetime(2022, 1, 1, 0, 0, 0)
+data_max = datetime.datetime(2023, 12, 31, 23, 59, 59)
 id_counter = 1
-
-for _ in range(499999):
-    id_prescricao_medica = id_counter
-    id_medicamento = random.randint(1, 150000)
-    ds_posologia = fake.sentence(nb_words=4)
-    ds_via = random.choice(["ORAL","PARENTAL","SUBCUTÂNEA","NASAL","RETAL","INTRA-VESICAL","NEBULIZAÇÃO","OCULAR"])
-    observacao_uso = fake.sentence(nb_words=6)
-    qt_medicamento = random.randint(1, 60)
-
-    cur.execute("SELECT ID_CONSULTA, ID_UNID_HOSPITAL FROM T_RHSTU_CONSULTA WHERE ID_CONSULTA = :ID_CONSULTA", ID_CONSULTA=id_counter)
-    result = cur.fetchone()
-    
-    id_unid_hospital = result[1]
-    id_consulta = result[0]
-
-    print(id_prescricao_medica, id_medicamento, ds_posologia, ds_via, observacao_uso, qt_medicamento,  id_unid_hospital, id_consulta)
-
-    cur.execute(
-        "INSERT INTO T_RHSTU_PRESCRICAO_MEDICA VALUES(:ID_PRESCRICAO_MEDICA, :ID_UNID_HOSPITAL, :ID_CONSULTA, :ID_MEDICAMENTO, :DS_POSOLOGIA, :DS_VIA, :DS_OBSERVACAO_USO, :QT_MEDICAMENTO, USER, SYSDATE)",
-        ID_PRESCRICAO_MEDICA=id_counter,
-        ID_UNID_HOSPITAL=id_unid_hospital,
-        ID_CONSULTA=id_consulta,
-        ID_MEDICAMENTO=id_medicamento,
-        DS_POSOLOGIA=ds_posologia,
-        DS_VIA=ds_via,
-        DS_OBSERVACAO_USO=observacao_uso,
-        QT_MEDICAMENTO=qt_medicamento
-    )
-    
-    print(id_counter)
-    id_counter += 1
-
-conn.commit()
-cur.close()
-conn.close()
+ 
+# Nome do arquivo CSV de saída
+output_file = 'D:/dados_prescricao.csv'
+ 
+with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
+    fieldnames = ['ID_UNID_HOSPITAL', 'ID_CONSULTA', 'ID_PACIENTE', 'ID_FUNC', 'DT_HR_CONSULTA', 'NR_CONSULTORIO']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+ 
+    writer.writeheader()
+ 
+    for _ in range(1, 10000000):
+        id_unid_hospital = random.randint(1, 20000)
+        id_consulta = id_counter
+        id_paciente = id_counter
+        id_funcionario = random.randint(1, 61000)
+        dt_hr_consulta = fake.date_time_between_dates(data_min, data_max).strftime('%d-%b-%Y %H:%M:%S').upper()
+        nr_consultorio = random.randint(1, 99999)
+ 
+        writer.writerow({
+            'ID_UNID_HOSPITAL': id_unid_hospital,
+            'ID_CONSULTA': id_consulta,
+            'ID_PACIENTE': id_paciente,
+            'ID_FUNC': id_funcionario,
+            'DT_HR_CONSULTA': dt_hr_consulta,
+            'NR_CONSULTORIO': nr_consultorio
+        })
+ 
+        print(id_counter)
+        id_counter += 1
+ 
+print(f'Dados gravados em {output_file}')
